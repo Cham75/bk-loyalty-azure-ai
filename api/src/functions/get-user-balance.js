20ca7a5770e544bbe3d264e6ee7892274ab980d6
Cham <hserraj75@gmail.com>
2025-12-05 08:53:33 +0100

    Add debug info to get-user-balance

--- api/src/functions/get-user-balance.js
+++ api/src/functions/get-user-balance.js
@@ -1,19 +1,36 @@
 const { app } = require("@azure/functions");
 const { getUser } = require("../data/db");
+const { isCosmosConfigured } = require("../data/cosmos-client");
 
 app.http("get-user-balance", {
   methods: ["GET"],
   authLevel: "anonymous",
   handler: async (request, context) => {
-    const userId = "demo-user-1"; // later: from B2C token
+    const userId = "demo-user-1";
 
-    const user = await getUser(userId);
+    try {
+      const user = await getUser(userId);
 
-    return {
-      jsonBody: {
-        userId: user.userId,
-        points: user.points,
-      },
-    };
+      return {
+        jsonBody: {
+          userId: user.userId,
+          points: user.points,
+          cosmosConfigured: isCosmosConfigured(),
+        },
+      };
+    } catch (err) {
+      context.log.error("Error in get-user-balance:", err);
+
+      // IMPORTANT: don't leak secrets, only basic info
+      return {
+        status: 500,
+        jsonBody: {
+          error: "INTERNAL_ERROR",
+          message: err.message || "Unknown error",
+          code: err.code || null,
+          cosmosConfigured: isCosmosConfigured(),
+        },
+      };
+    }
   },
 });
